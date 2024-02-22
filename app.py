c57e0febe73b1c77809ee549b99723ba8a00bfd5
Adrian McBroom <adrianmcb2000@gmail.com>
2024-02-22 12:33:54 -0800

    started redirections

--- app.py
+++ app.py
@@ -6,4 +6,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
+responses = []
+
 debug = DebugToolbarExtension(app)
+
+
+@app.get('/')
+def start_survey():
+    return render_template('survey_start.html', title=survey.title, instructions=survey.instructions)
+
+
+@app.post('/begin')
+def redirect_to_first_question():
+    return redirect('/questions/0')
+
+
+@app.get('/questions/<int:question_num>')
+def redirect_to_next_question(question_num):
+    return render_template('question.html', question=survey.questions[question_num])
