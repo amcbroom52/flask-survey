a836d9a498665eadcc02eed7229125c455e3de00
Adrian McBroom <adrianmcb2000@gmail.com>
2024-02-22 16:18:57 -0800

    Finished first code review

--- app.py
+++ app.py
@@ -13,23 +13,42 @@ debug = DebugToolbarExtension(app)
 
 @app.get('/')
 def start_survey():
-    return render_template('survey_start.html', title=survey.title, instructions=survey.instructions)
+    """Renders start survey page"""
+    return render_template('survey_start.html', title=survey.title, instructions=survey.instructions)  # Send Whole Surevy
 
 
 @app.post('/begin')
 def redirect_to_first_question():
+    """Sends redirect to the first question page"""
     return redirect('/questions/0')
 
 
 @app.get('/questions/<int:question_num>')
 def render_question(question_num):
+    """Renders page for a specific question"""
+    if question_num > len(responses):
+        return redirect(f'/questions/{len(responses)}')
     return render_template('question.html', question=survey.questions[question_num])
+
 
 @app.post('/answer')
 def redirect_to_next_question():
+    """Sends redirect to the next unanswered question"""
     # dot notation to access property form and bracket notation to access the key
     answer = request.form['answer']
     responses.append(answer)
+    print(f"**********************************{responses}")
+
+    if len(survey.questions) <= len(responses):
+        return redirect('/completion')
 
     return redirect(
         f'/questions/{len(responses)}')
+
+
+@app.get('/completion')
+def show_completion():
+    """Renders Completion page"""
+    if len(responses) != len(survey.questions):
+        return redirect(f'/questions/{len(responses)}')
+    return render_template('completion.html', responses=responses, questions=survey.questions)
